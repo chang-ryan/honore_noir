23dba09c1aacec6883480dd5e57825ab495411b5
Ryan Chang <ryan.chang@hyfn.com>
2017-08-17 15:54:32 -0700

    add bg color

---
+++ src/scripts/sections/header.js
@@ -0,0 +1,15 @@
+/**
+ * Header Section Script
+ * -----------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Header section.
+ *
+ * @namespace header
+ */
+
+theme.Header = {
+  var selectors = {
+    menuButton: 'menu'
+  };
+
+
+};
